21dfde30889338fa293f1f9ef8b9f4ba01c3268f
EC2 Default User <ec2-user@ip-172-31-37-44.us-east-2.compute.internal>
2018-11-29 01:10:38 +0000

    started only create the top header

---
+++ starter-code/src/components/Nav.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+class Nav extends Component {
+    render(){
+        return(
+            <div className="nav">
+                <h1>Wiki Countries</h1>
+            </div>
+        )
+    }
+}
+
+export default Nav;
